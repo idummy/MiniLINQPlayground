aba6b80edbbf7d7caf761b4f6d6fb9d8f3285880
Roshan <rosh.sharma281@gmail.com>
2025-08-05 23:42:11 +0530

    Fix: Added project folder as normal folder, not submodule

---
+++ MiniLINQPlayground/Logic/MiniWhere.cs
@@ -0,0 +1,15 @@
+namespace MiniLINQPlayground.Logic
+{
+    public static class MiniWhereExtention
+    {
+        public static IEnumerable<T> MiniWhere<T>(this IEnumerable<T> source, Func<T, bool> predicate)
+        {
+            foreach (var item in source)
+            {
+                if (predicate(item))
+                    yield return item;
+            }
+        }
+    }
+}
+//
---
+++ MiniLINQPlayground/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MiniLINQPlayground.Logic;
+
+var numbers = new List<int> { 1, 2, 3, 4, 5 };
+
+// Mini Where Test
+var evens = numbers.MiniWhere(n => n % 2 == 0);
+Console.WriteLine("Even numbers:");
+foreach (var n in evens)
+{
+    Console.WriteLine(n);
+}
+
